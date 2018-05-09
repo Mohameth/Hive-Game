e8372945f9dfbebde1fd21738a80a12943925656
trousssi <boby38119@live.fr>
2018-05-09 11:07:32 +0200

    test case 100%

---
+++ Hive/test/Modele/CaseTest.java
@@ -0,0 +1,343 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+import Modele.Insectes.Fourmi;
+import Modele.Insectes.Insecte;
+import Modele.Insectes.Reine;
+import Modele.Insectes.Scarabee;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author boby
+ */
+public class CaseTest {
+
+    public CaseTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("=============================================");
+        System.out.println("====               TEST CASE             ====");
+        System.out.println("=============================================");
+        System.out.println("");
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of estVide method, of class Case.
+     */
+    @Test
+    public void testEstVide() {
+        System.out.println("=============================================");
+        System.out.println("TEST estVide : =============================>\n");
+        Case instance = new Case(new Point3DH(0, 0, 0));
+        System.out.println("Test sur une case vide :");
+        assertTrue(instance.estVide());
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        System.out.println("ajout d'un insecte :");
+        try {
+            instance.addInsecte(new Reine(new JoueurHumain(new Plateau())));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        } catch (Exception ex) {
+            fail("exception anormal");
+        }
+        System.out.println("Test sur une case non vide :");
+        assertFalse(instance.estVide());
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        try {
+            instance.removeInsecte();
+        } catch (Exception ex) {
+            fail("exeption anormal");
+        }
+
+        System.out.println("Test aprés avoir supprimé l'insecte :");
+        assertTrue(instance.estVide());
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        System.out.println("");
+    }
+
+    /**
+     * Test of addInsecte method, of class Case.
+     */
+    @Test
+    public void testAddRemInsecte() {
+        System.out.println("=============================================");
+        System.out.println("TEST ajout suppretion d'Insecte : ==========>\n");
+        JoueurHumain j = new JoueurHumain(new Plateau());
+        Reine reine = new Reine(j);
+        Case instance = new Case(new Point3DH(0, 0, 0));
+        try {
+            System.out.println("ajout de la reine :");
+            instance.addInsecte(reine);
+        } catch (Exception e) {
+            System.out.println("\u001B[31m" + "\t Failed ✖\n");
+            fail("ne doit pas lever d'exception");
+        }
+        assertEquals(reine, instance.getInsectes().get(0));
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        Fourmi fourmi = new Fourmi(j);
+        Scarabee scarabee = new Scarabee(j);
+        try {
+            System.out.println("ajout d'une fourmis sur une case non vide : doit echouer (Exception) :");
+            instance.addInsecte(fourmi); // doit lever une exeption il faut enlever l'insecte avant d'en ajouté un autre -> evite qu'un insecte soit supprimé du jeu
+            System.out.println("\u001B[31m" + "\t Failed ✖\n");
+            fail("aucune exeption levé");
+        } catch (Exception e) {
+            assert (e.getMessage().equals("Ajout impossible sur case non vide"));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        }
+
+        try {
+            System.out.println("ajout d'un scarabee sur une case non vide :");
+            instance.addInsecte(scarabee); // doit lever une exeption il faut enlever l'insecte avant d'en ajouté un autre -> evite qu'un insecte soit supprimé du jeu
+            assertEquals(scarabee, instance.getInsectes().get(1));
+            assertEquals(reine, instance.getInsectes().get(0));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+            System.out.println("supression du scarabee :");
+            instance.removeInsecte();
+            assertEquals(reine, instance.getInsectes().get(0));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        } catch (Exception e) {
+            fail("exception annormal");
+        }
+
+        try {
+            System.out.println("supression de la reine :");
+            instance.removeInsecte();
+            assertTrue(instance.estVide());
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        } catch (Exception e) {
+            fail("exception annormal");
+        }
+
+        try {
+            System.out.println("ajout d'une fourmis sur case vide:");
+            instance.addInsecte(fourmi);
+        } catch (Exception e) {
+            System.out.println("\u001B[31m" + "\t Failed ✖\n");
+            fail("ne doit pas lever d'exception");
+        }
+        assertEquals(fourmi, instance.getInsectes().get(0));
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        try {
+            System.out.println("supression de la fourmi :");
+            instance.removeInsecte();
+            assertTrue(instance.estVide());
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        } catch (Exception e) {
+            fail("exception annormal");
+        }
+
+        ArrayList<Insecte> li = new ArrayList<>();
+        li.add(new Fourmi(j));
+        li.add(new Scarabee(j));
+        li.add(new Scarabee(j));
+        li.add(new Scarabee(j));
+        li.add(new Scarabee(j));
+        li.add(new Scarabee(j));
+
+        try {
+            System.out.println("ajout de 5 insectes sur la même case (1 fourmi,4 scarabee) :");
+            instance.addInsecte(li.get(0));
+            instance.addInsecte(li.get(1));
+            instance.addInsecte(li.get(2));
+            instance.addInsecte(li.get(3));
+            instance.addInsecte(li.get(4));
+
+            assertEquals(li.get(0), instance.getInsectes().get(0));
+            assertEquals(li.get(1), instance.getInsectes().get(1));
+            assertEquals(li.get(2), instance.getInsectes().get(2));
+            assertEquals(li.get(3), instance.getInsectes().get(3));
+            assertEquals(li.get(4), instance.getInsectes().get(4));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        } catch (Exception e) {
+            System.out.println("\u001B[31m" + "\t Failed ✖\n");
+            fail("exception anormal");
+        }
+
+        try {
+            System.out.println("ajout d'un 6e insecte (doit echouer) :");
+            instance.addInsecte(li.get(5));
+            System.out.println("\u001B[31m" + "\t Failed ✖\n");
+        } catch (Exception e) {
+            assert (e.getMessage().equals("Ajout impossible -> 5 insectes maximum"));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        }
+
+        System.out.println("test getInsectOnTop :");
+        assertEquals(li.get(4), instance.getInsecteOnTop());
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        try {
+            System.out.println("supression des insectes :");
+
+            instance.removeInsecte();
+
+            assertEquals(li.get(0), instance.getInsectes().get(0));
+            assertEquals(li.get(1), instance.getInsectes().get(1));
+            assertEquals(li.get(2), instance.getInsectes().get(2));
+            assertEquals(li.get(3), instance.getInsectes().get(3));
+
+            assertEquals(li.get(3), instance.getInsecteOnTop());
+
+            instance.removeInsecte();
+
+            assertEquals(li.get(0), instance.getInsectes().get(0));
+            assertEquals(li.get(1), instance.getInsectes().get(1));
+            assertEquals(li.get(2), instance.getInsectes().get(2));
+
+            assertEquals(li.get(2), instance.getInsecteOnTop());
+
+            instance.removeInsecte();
+
+            assertEquals(li.get(0), instance.getInsectes().get(0));
+            assertEquals(li.get(1), instance.getInsectes().get(1));
+
+            assertEquals(li.get(1), instance.getInsecteOnTop());
+
+            instance.removeInsecte();
+
+            assertEquals(li.get(0), instance.getInsectes().get(0));
+
+            assertEquals(li.get(0), instance.getInsecteOnTop());
+
+            instance.removeInsecte();
+
+            assertTrue(instance.estVide());
+
+            assertEquals(null, instance.getInsecteOnTop());
+
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        } catch (Exception e) {
+            fail("exception anormal");
+        }
+
+    }
+
+    /**
+     * Test of insecteBloque method, of class Case.
+     */
+    @Test
+    public void testInsecteBloque() {
+        System.out.println("=============================================");
+        System.out.println("TEST insecteBloque : =======================>\n");
+        Fourmi fourmi = new Fourmi(new JoueurHumain(new Plateau()));
+        Scarabee scarabee1 = new Scarabee(new JoueurHumain(new Plateau()));
+        Scarabee scarabee2 = new Scarabee(new JoueurHumain(new Plateau()));
+        Case instance = new Case(new Point3DH(0, 0, 0));
+        System.out.println("test sur une case vide (doit lever une exeption) :");
+        try {
+            instance.insecteBloque(fourmi);
+            System.out.println("\u001B[31m" + "\t Failed ✖\n");
+            fail("exception non lever");
+        } catch (Exception e) {
+            assert (e.getMessage().equals("Case vide"));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        }
+
+        System.out.println("test avec la fourmi après l'avoir posé sur la case :");
+        try {
+            instance.addInsecte(fourmi);
+            assertFalse(instance.insecteBloque(fourmi));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        } catch (Exception e) {
+            fail("exception anormal");
+        }
+
+        System.out.println("test avec le scarabee alors qu'il n'est pas sur la case :");
+        try {
+            instance.insecteBloque(scarabee1);
+            System.out.println("\u001B[31m" + "\t Failed ✖\n");
+        } catch (Exception e) {
+            assert (e.getMessage().equals("Cet insecte n'est pas sur cette case"));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        }
+
+        System.out.println("test avec le scarabee sur la case (au dessus de la fourmi) :");
+        try {
+            instance.addInsecte(scarabee1);
+            assertFalse(instance.insecteBloque(scarabee1));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+            
+            System.out.println("test avec la fourmis alors que le scarabee est au dessus :");
+            assertTrue(instance.insecteBloque(fourmi));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+            
+            System.out.println("test avec un 2eme scarabee au dessus du premier et de la fourmis :");
+            instance.addInsecte(scarabee2);
+            assertFalse(instance.insecteBloque(scarabee2));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+            
+            System.out.println("test sur le 1er scarabee :");
+            assertTrue(instance.insecteBloque(scarabee1));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+            
+            System.out.println("test sur la fourmis");
+            assertTrue(instance.insecteBloque(fourmi));
+            System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        } catch (Exception e) {
+            fail("exception anormal");
+        }
+        
+        System.out.println("");
+    }
+
+    /**
+     * Test of equals method, of class Case.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("=============================================");
+        System.out.println("TEST equals : ==============================>\n");
+        Case instance = new Case(new Point3DH(0, 0, 0));
+        System.out.println("test avec une case ayant les mêmes coordonees :");
+        assertTrue(instance.equals(new Case(new Point3DH(0, 0, 0))));
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        System.out.println("test avec la même case :");
+        assertTrue(instance.equals(instance));
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        System.out.println("test avec null :");
+        assertFalse(instance.equals(null));
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        System.out.println("test avec un objet d'une autre classe :");
+        assertFalse(instance.equals(new Plateau()));
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+        System.out.println("test avec une case n'ayant pas les mêmes coordonees :");
+        assertFalse(instance.equals(new Case(new Point3DH(0, 5, 4))));
+        System.out.println("\u001B[32m" + "\t Passed ✔ \n");
+
+        System.out.println("");
+    }
+}
---
+++ Hive/test/Modele/Insectes/InsecteTest.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele.Insectes;
+
+import Modele.Case;
+import Modele.Joueur;
+import Modele.JoueurHumain;
+import Modele.Plateau;
+import Modele.Point3DH;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author boby
+ */
+public class InsecteTest {
+    
+    public InsecteTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("=============================================");
+        System.out.println("====           TEST INSECTE              ====");
+        System.out.println("=============================================");
+        System.out.println("");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    /**
+     * TODO : Put into other test file Test of deplacementPossible method, of
+     * class Reine.
+     */
+    @Test
+    public void testDeplacementReine() {
+        System.out.println("=============================================");
+        System.out.println("Test deplacementReine ======================>\n");
+        Point3DH point = new Point3DH(0, 0, 0);
+        Plateau instance = new Plateau();
+        Joueur j1 = new JoueurHumain(instance);
+
+        Case caseReine = instance.getCase(point);
+        Reine r = new Reine(j1);
+        r.setEmplacement(caseReine);
+
+        ArrayList<Case> result = new ArrayList<>();
+        result.addAll(r.deplacementPossible(instance));
+
+        ArrayList<Point3DH> expected = new ArrayList<>();
+        expected.add(new Point3DH(0, +1, -1));
+        expected.add(new Point3DH(+1, 0, -1));
+        expected.add(new Point3DH(+1, -1, 0));
+        expected.add(new Point3DH(0, -1, +1));
+        expected.add(new Point3DH(-1, 0, +1));
+        expected.add(new Point3DH(-1, +1, 0));
+
+        arrayCorresponds(result, expected);
+
+        //TEST GATE
+        ArrayList<Point3DH> expectedWithGate = new ArrayList<>();
+        //expectedWithGate.add(new Point3DH(0, +1, -1)); //--> Case occupe
+        //expectedWithGate.add(new Point3DH(+1, 0, -1)); //--> Inaccessible (gate)
+        //expectedWithGate.add(new Point3DH(+1, -1, 0)); //--> Case occupe
+        expectedWithGate.add(new Point3DH(0, -1, +1));
+        expectedWithGate.add(new Point3DH(-1, 0, +1));
+        expectedWithGate.add(new Point3DH(-1, +1, 0));
+
+        Case caseOccupe1 = instance.getCase(new Point3DH(0, 1, -1));
+        Case caseOccupe2 = instance.getCase(new Point3DH(1, -1, 0));
+
+        try {
+            caseOccupe1.addInsecte(new Fourmi(j1));
+            caseOccupe2.addInsecte(new Fourmi(j1));
+        } catch (Exception ex) {
+            fail();
+        }
+
+        result = new ArrayList<>();
+        result.addAll(r.deplacementPossible(instance));
+
+        arrayCorresponds(result, expectedWithGate);
+
+    }
+
+    private void arrayCorresponds(ArrayList<Case> result, ArrayList<Point3DH> pointsExpected) {
+        assertTrue(pointsExpected.size() == result.size());
+        for (Case c : result) {
+            assertTrue(pointsExpected.contains(c.getCoordonnees()));
+            pointsExpected.remove(c.getCoordonnees());
+        }
+
+        assertTrue(pointsExpected.isEmpty());
+    }
+    
+}
