e38b017d1aedc23241b84fdafefbf7eb90da51b4
Mohameth <mohameth38@outlook.fr>
2018-05-21 15:20:52 +0200

    factorisation

--- Hive/src/Modele/Insectes/Scarabee.java
+++ Hive/src/Modele/Insectes/Scarabee.java
@@ -20,27 +20,10 @@ public class Scarabee extends Insecte {
     
     @Override
     public Collection<Case> deplacementPossible(Plateau plateau) {
-        /*if(plateau.rucheBrisee2(this.getEmplacement())) {
-        	return new ArrayList<>();
-        }*/
+        
         if (!this.getJoueur().tousPionsPosables()) return new ArrayList<>();
         
         return plateau.getCasesVoisinesAccessibles(this.getEmplacement(), false);
-		
-		/*if(plateau.rucheBrisee2(this.getEmplacement())) {
-        	return new ArrayList<>();
-        }
-    	
-    	ArrayList<Case> casePossibles=(ArrayList<Case>) plateau.getCasesVoisinesOccupees(this.getEmplacement());
-    	ArrayList<Case> casePossibles2=(ArrayList<Case>) plateau.getCasesVoisines(this.getEmplacement(),true);
-    	
-    	for(int j=0;j<casePossibles2.size();j++) {
-			
-			if(plateau.glissementPossibles(this.getEmplacement(),casePossibles2.get(j))) {
-				casePossibles.add(casePossibles2.get(j));
-			}
-		}
-    	return casePossibles;*/
     }
 
     @Override
