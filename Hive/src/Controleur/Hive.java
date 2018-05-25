6061caea99a7d2a19d28e6b3de7075a315e3cd56
trousssi <simon.troussier@outlook.fr>
2018-05-25 15:37:24 +0200

    fix Undo 1 Seul possible

--- Hive/src/Controleur/Hive.java
+++ Hive/src/Controleur/Hive.java
@@ -32,6 +32,7 @@ public class Hive implements Serializable {
     Joueur joueur1;
     Joueur joueur2;
     Joueur joueurCourant;
+    boolean Undo = true;
     transient Observer o;
 
     public Hive(String[] args) {
@@ -195,20 +196,18 @@ public class Hive implements Serializable {
         if (this.joueurCourant.getNumJoueur().estHumain()) {
             this.plateau.notifieVue(-1);
         } else {
-            this.plateau.notifieVue(((JoueurIA)this.joueurCourant).getTempsRestant());
+            this.plateau.notifieVue(((JoueurIA) this.joueurCourant).getTempsRestant());
         }
-        
-        
+
         if (joueurCourant.equals(this.joueur1)) {
             this.joueurCourant = this.joueur2;
         } else if (joueurCourant.equals(this.joueur2)) {
             this.joueurCourant = this.joueur1;
         }
         if (!this.joueurCourant.getNumJoueur().estHumain()) {
-                ((JoueurIA) this.joueurCourant).coup(null, null);
-                this.joueurSuivant();
+            ((JoueurIA) this.joueurCourant).coup(null, null);
+            this.joueurSuivant();
         }
-
 
     }
 
@@ -289,6 +288,7 @@ public class Hive implements Serializable {
                 this.joueur1.setPlateau(plateau);
                 this.joueur2.setPlateau(plateau);
                 this.joueurCourant.setPlateau(plateau);
+                this.Undo = h.Undo;
 
                 this.plateau.notifieVue(-1);
 
@@ -302,12 +302,16 @@ public class Hive implements Serializable {
     }
 
     public boolean UndoPossible() {
-        if (this.joueur2 instanceof JoueurIA) {
-            return this.joueur1.UndoPossible();
-        } else if (this.joueurCourant.equals(this.joueur1)) {
-            return this.joueur2.UndoPossible();
+        if (Undo) {
+            if (this.joueur2 instanceof JoueurIA) {
+                return this.joueur1.UndoPossible();
+            } else if (this.joueurCourant.equals(this.joueur1)) {
+                return this.joueur2.UndoPossible();
+            } else {
+                return this.joueur1.UndoPossible();
+            }
         } else {
-            return this.joueur1.UndoPossible();
+            return false;
         }
     }
 
@@ -323,6 +327,7 @@ public class Hive implements Serializable {
                 this.joueur1.Undo();
                 this.joueurCourant = this.joueur1;
             }
+            this.Undo = false;
             this.plateau.notifieVue(-1);
         }
     }
@@ -349,6 +354,7 @@ public class Hive implements Serializable {
                 this.joueur2.Redo();
                 this.joueurCourant = this.joueur1;
             }
+            this.Undo = true;
             this.plateau.notifieVue(-1);
         }
     }
