0fb4b63a19c2e3df819e9cf8fd061a30e6da1232
Syl-47 <sylvestre.wampach@gmail.com>
2018-05-28 14:57:41 +0200

    blocage undo redo btn ia

--- Hive/src/Vue/VueTerrain.java
+++ Hive/src/Vue/VueTerrain.java
@@ -874,6 +874,8 @@ public class VueTerrain extends Vue implements ObservateurVue, Observer {
             updateMainJoueurColor(false);
             lockTousLespions();
             highlightPlayeName();
+            this.bRedo.setDisable(true);
+            this.bUndo.setDisable(true);
         }
     }
 
@@ -1507,7 +1509,6 @@ public class VueTerrain extends Vue implements ObservateurVue, Observer {
         Plateau p = (Plateau) o;
         this.pModel = p;
         long tempsRestant = (long) arg;
-
         //si >0 alors c'est une ia
         if (tempsRestant > 0) {
             System.out.println("IA IS THINKING");
@@ -1516,8 +1517,6 @@ public class VueTerrain extends Vue implements ObservateurVue, Observer {
             this.bUndo.setDisable(true);
         } else { //l'humain a joué puis directe après l'ia va jouer
             this.iaCanPlay = -1;
-            //updateMainJoueur();
-            //reconstructionPlateau(this.pModel);
         }
 
     }
@@ -1566,12 +1565,10 @@ public class VueTerrain extends Vue implements ObservateurVue, Observer {
         hideZoneLibre();
         this.controleur.setUndo(true);
         hudToFront();
-        //updateMainJoueur();
-        this.controleur.joueurSuivant();
-        updateMainJoueur();
-        updateUndoRedoBtn();
-
         CheckGagnant();
+        this.controleur.joueurSuivant();
+        updateUndoRedoBtn();
+        updateMainJoueur();
     }
 
     public void ordinateurJoue() {
@@ -1594,12 +1591,12 @@ public class VueTerrain extends Vue implements ObservateurVue, Observer {
             this.currentSelected = this.listPionsPlateau.get(iaMove.getOrig());
             updateMousePressedZoneLibre(getZoneLibreEgal(iaMove.getCible()));
         }
-        updateUndoRedoBtn();
         hudToFront();
+        CheckGagnant();
         this.controleur.joueurSuivant();
+        updateUndoRedoBtn();
         updateMainJoueur();
         reconstructionPlateau(this.pModel);
-        CheckGagnant();
     }
 
     public void applyUndo(Deplacement depl) {
